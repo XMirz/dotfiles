528c071a02818b6c06d37b306c5e3829daec8fd7
xmirz <linearattack0@gmail.com>
2022-03-22 01:03:30 +0700

    added battery warning , remove unused blob

--- bspwm/bin/ss_window.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-import -window $(xdotool getactivewindow) -border $HOME/Pictures/Screenshots/Screenshot.png;
-
-convert $HOME/Pictures/Screenshots/Screenshot.png +clone -background black -shadow 50x10+0+10 +swap -background none -layers merge +repage $HOME/Pictures/Screenshots/Screenshot.png;
-
-convert $HOME/Pictures/Screenshots/Screenshot.png -bordercolor none -border 30 $HOME/Pictures/Screenshots/Screenshot_$(date '+%d_%m_%Y.%H%M_%S').png && rm $HOME/Pictures/Screenshots/Screenshot.png
